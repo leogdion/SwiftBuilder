a6d3e1dfa416a75f08215fecc9043839c1e129f8
Leo Dion <leogdion@brightdigit.com>
2025-06-16 21:34:22 -0400

    fixing ci

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "SyntaxKit",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v13),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+        .visionOS(.v1)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
