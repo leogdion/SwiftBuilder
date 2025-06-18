ea667a4118d9abf90a30c4e1605eb70807fe59f9
Leo Dion <leogdion@brightdigit.com>
2025-06-18 13:35:50 -0400

    [skip ci]

---
+++ Examples/Remaining/attributes/dsl.swift
@@ -0,0 +1,9 @@
+Class("Foo") {
+    Variable(.var, name: "bar", type: "String", defaultValue: "bar").attribute("Published")
+    Function("bar") {
+        print("bar")
+    }.attribute("available", arguments: ["iOS 17.0", "*"])
+    Function("baz") {
+        print("baz")
+    }.attribute("MainActor")    
+}.attribute("objc")
