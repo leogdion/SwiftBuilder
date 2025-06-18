6e6e5205a37d0ea1344afd49417d9ea5e1e017c2
leogdion <leogdion@brightdigit.com>
2025-06-18 13:43:13 -0400

    fixing long file

--- Tests/SyntaxKitTests/ProtocolTests.swift
+++ Tests/SyntaxKitTests/ProtocolTests.swift
@@ -2,7 +2,7 @@ import Testing
 
 @testable import SyntaxKit
 
-struct ClassAndProtocolTests {
+struct ProtocolTests {
   @Test func testSimpleProtocol() {
     let vehicleProtocol = Protocol("Vehicle") {
       PropertyRequirement("numberOfWheels", type: "Int", access: .get)
@@ -23,24 +23,6 @@ struct ClassAndProtocolTests {
       """
 
     let normalizedGenerated = vehicleProtocol.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithInheritance() {
-    let carClass = Class("Car") {
-      Variable(.var, name: "brand", type: "String")
-      Variable(.var, name: "numberOfWheels", type: "Int")
-    }.inherits("Vehicle")
-
-    let expected = """
-      class Car: Vehicle {
-          var brand: String
-          var numberOfWheels: Int
-      }
-      """
-
-    let normalizedGenerated = carClass.generateCode().normalize()
     let normalizedExpected = expected.normalize()
     #expect(normalizedGenerated == normalizedExpected)
   }
@@ -121,142 +103,6 @@ struct ClassAndProtocolTests {
       """
 
     let normalizedGenerated = protocolWithMutatingFunction.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testEmptyClass() {
-    let emptyClass = Class("EmptyClass") {}
-
-    let expected = """
-      class EmptyClass {
-      }
-      """
-
-    let normalizedGenerated = emptyClass.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithGenerics() {
-    let genericClass = Class("Container", generics: ["T"]) {
-      Variable(.var, name: "value", type: "T")
-    }
-
-    let expected = """
-      class Container<T> {
-          var value: T
-      }
-      """
-
-    let normalizedGenerated = genericClass.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithMultipleGenerics() {
-    let multiGenericClass = Class("Pair", generics: ["T", "U"]) {
-      Variable(.var, name: "first", type: "T")
-      Variable(.var, name: "second", type: "U")
-    }
-
-    let expected = """
-      class Pair<T, U> {
-          var first: T
-          var second: U
-      }
-      """
-
-    let normalizedGenerated = multiGenericClass.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testFinalClass() {
-    let finalClass = Class("FinalClass") {
-      Variable(.var, name: "value", type: "String")
-    }.final()
-
-    let expected = """
-      final class FinalClass {
-          var value: String
-      }
-      """
-
-    let normalizedGenerated = finalClass.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithMultipleInheritance() {
-    let classWithMultipleInheritance = Class("AdvancedVehicle") {
-      Variable(.var, name: "speed", type: "Int")
-    }.inherits("Vehicle", "Codable", "Equatable")
-
-    let expected = """
-      class AdvancedVehicle: Vehicle, Codable, Equatable {
-          var speed: Int
-      }
-      """
-
-    let normalizedGenerated = classWithMultipleInheritance.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithGenericsAndInheritance() {
-    let genericClassWithInheritance = Class("GenericContainer", generics: ["T"]) {
-      Variable(.var, name: "items", type: "[T]")
-    }.inherits("Collection")
-
-    let expected = """
-      class GenericContainer<T>: Collection {
-          var items: [T]
-      }
-      """
-
-    let normalizedGenerated = genericClassWithInheritance.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testFinalClassWithInheritanceAndGenerics() {
-    let finalGenericClass = Class("FinalGenericClass", generics: ["T"]) {
-      Variable(.var, name: "value", type: "T")
-    }.inherits("BaseClass").final()
-
-    let expected = """
-      final class FinalGenericClass<T>: BaseClass {
-          var value: T
-      }
-      """
-
-    let normalizedGenerated = finalGenericClass.generateCode().normalize()
-    let normalizedExpected = expected.normalize()
-    #expect(normalizedGenerated == normalizedExpected)
-  }
-
-  @Test func testClassWithFunctions() {
-    let classWithFunctions = Class("Calculator") {
-      Function("add", returns: "Int") {
-        Parameter(name: "a", type: "Int")
-        Parameter(name: "b", type: "Int")
-      } _: {
-        Return {
-          VariableExp("a + b")
-        }
-      }
-    }
-
-    let expected = """
-      class Calculator {
-          func add(a: Int, b: Int) -> Int {
-              return a + b
-          }
-      }
-      """
-
-    let normalizedGenerated = classWithFunctions.generateCode().normalize()
     let normalizedExpected = expected.normalize()
     #expect(normalizedGenerated == normalizedExpected)
   }
